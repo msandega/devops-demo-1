4acc6856477207df9d22bf1a05e4a978caec83a0
msandega <ingato.andega@gmail.com>
2025-09-01 17:50:39 +0300

    qh

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,8 @@
-from app.app import app
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from app import app
 
 def test_health():
     client = app.test_client()
